5399ec6e100e28dbcec4e775e850a1859f2a24fa
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2026-02-25 14:39:32 -0300

    Add files via upload

---
+++ UC30 - 2026 02 25 (PY)/inicio.py
@@ -0,0 +1,12 @@
+#4 wsbfygc vuftv#yvbbbbbbbbbbbgfyitryseziohjk
+
+nome = "Juh V"
+idade = 16
+pi_value = 3.14 #Não sei minha altura
+matricula = True
+nome = "Jeej"
+
+print(nome, "Jeej")
+print(idade)
+print(pi_value - 2)
+print(matricula)
---
+++ UC30 - 2026 02 25 (PY)/tets.py
@@ -0,0 +1,7 @@
+piValueMinusTwo = 1.14
+Var1 = 10
+var1 = 12
+
+print(type(piValueMinusTwo + 2))
+print(type(var1 -2))
+print(type(Var1 -8))
