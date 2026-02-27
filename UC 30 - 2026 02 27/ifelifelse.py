d51e5598f711dbe47b11cda9afaba20418aa444d
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2026-02-27 16:07:30 -0300

    Add files via upload

---
+++ UC 30 - 2026 02 27/ifelifelse.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+
+nome = input("Tell me your name:")
+if len(nome) > 50:
+    print("too big!")
+print(f"your name have {len(nome)} characters.")
+
+idade = int(input("Idade aqui: "))
+if idade >=16 and idade <+ 69:
+    print("Permitido")
+else:
+    print("Não permitido")
---
+++ UC 30 - 2026 02 27/test.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+
+altura = float(input("Altura de: "))
+altura = altura * 100
+
+print(f"Altura de: {altura} cm...")
+
+nome = "Juh"
+idade = "16"
+
+texto = "Meu nome {} e idade {}".format(nome, idade)
+print(texto)
