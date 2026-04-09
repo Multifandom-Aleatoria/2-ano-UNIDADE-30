d0b2a909a972ba61546273599e8b3a651c686b8a
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2026-04-09 13:44:22 -0300

    Introduce basic variables and arithmetic in TIUC30.py
    Added basic variable examples and arithmetic operations in Python.

--- TIUC30.py
+++ TIUC30.py
@@ -1,14 +1,22 @@
 print("Hello World!")
 
+#As variavéis básicas do Phyton:
 Num = 4102224 #Variavál tipo número inteiro (1, 2, 3...).
 Name = "TestName01" #Variavél tipo string (texto).
 PiValue = 3.14 #Variavél tipo número real (como números decimais).
 HaveReadABookToday = False #Variavél tipo bool (verdadeiro ou falso).
 
 #Variavéis são valores definidos pelo programador ou digitados pelo usuário que são usados para armazenar informações e dados importantes.
+#As Variavéis só podem conter letras antes de declarar seu valor, uma variavél não funciona se for com números. Por exemplo: 3 = 3.
 
 print("Valor pra testes: ", Num, type(Num))
 print("Nome: ", Name, type(Name))
 print("Valor aprox. de Pi: ", PiValue, type(PiValue))
 print("Já leu um livro hoje: ", HaveReadABookToday, type(HaveReadABookToday))
-print("\n Para dar novos valores e atrubuir eles a variáveis, é preciso usar alguns operadores, como: ")
+
+#Essa função type() identifica o tipo da variável digitada
+#Operadores aritmétricos são usados para duas funções:
+
+#Primeira fução:
+NewPiValue = PiValue + 3 
+print(NewPiValue)
