b0a3a6b072f00ef068442e13c278b344e75a1210
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2026-04-09 14:44:16 -0300

    Fix formatting of string variable assignment

--- TIUC30.py
+++ TIUC30.py
@@ -2,7 +2,7 @@ print("Hello World!")
 
 #As variavéis básicas do Phyton:
 Num = 4102224 #Variavál tipo número inteiro (1, 2, 3...).
-Name = "TestName01" #Variavél tipo string (texto).
+Name = "TestName01" #Variavél tipo string (texto). Apenas é considerada se tiver entre aspas
 PiValue = 3.14 #Variavél tipo número real (como números decimais).
 HaveReadABookToday = False #Variavél tipo bool (verdadeiro ou falso).
 
