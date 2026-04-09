897663fe409916f3f4761de94f76a0c3d6681dda
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2026-04-09 03:01:21 -0300

    Add files via upload

---
+++ TIUC30.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+
+Num = 4102224 #Variavál tipo número inteiro (1, 2, 3...).
+Name = "TestName01" #Variavél tipo string (texto).
+PiValue = 3.14 #Variavél tipo número real (como números decimais).
+HaveReadABookToday = False #Variavél tipo bool (verdadeiro ou falso).
+
+#Variavéis são valores definidos pelo programador ou digitados pelo usuário que são usados para armazenar informações e dados importantes.
+
+print("Valor pra testes: ", Num, type(Num))
+print("Nome: ", Name, type(Name))
+print("Valor aprox. de Pi: ", PiValue, type(PiValue))
+print("Já leu um livro hoje: ", HaveReadABookToday, type(HaveReadABookToday))
+print("\n Para dar novos valores e atrubuir eles a variáveis, é preciso usar alguns operadores, como: ")
