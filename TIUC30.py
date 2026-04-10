f7985b456f542df71f08ee6bc13814ec6bde0ad3
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2026-04-10 11:06:52 -0300

    Refactor variable comments and print statements
    Updated comments and print statements for clarity and corrected variable descriptions.

--- TIUC30.py
+++ TIUC30.py
@@ -1,15 +1,50 @@
 print("Hello World!")
 
-#As variavéis básicas do Phyton:
-Num = 4102224 #Variavál tipo número inteiro (1, 2, 3...).
-Name = "TestName01" #Variavél tipo string (texto). Apenas é considerada se tiver entre aspas
-PiValue = 3.14 #Variavél tipo número real (como números decimais).
-HaveReadABookToday = False #Variavél tipo bool (verdadeiro ou falso).
+# As variáveis básicas do Python:
 
-#Variavéis são valores definidos pelo programador ou digitados pelo usuário que são usados para armazenar informações e dados importantes.
-#As Variavéis só podem conter letras antes de declarar seu valor, uma variavél não funciona se for com números. Por exemplo: 3 = 3.
+Num = 4102224  # Variável tipo número inteiro (1, 2, 3...).
+Name = "TestName01"  # Variável tipo string (texto). Apenas é considerada se tiver entre aspas.
+PiValue = 3.14  # Variável tipo número real (como números decimais).
+HaveReadABookToday = False  # Variável tipo bool (verdadeiro ou falso).
 
-print("Valor pra testes: ", Num, type(Num))
-print("Nome: ", Name, type(Name))
-print("Valor aprox. de Pi: ", PiValue, type(PiValue))
-print("Já leu um livro hoje: ", HaveReadABookToday, type(HaveReadABookToday))
+# Variáveis são valores definidos pelo programador ou digitados pelo usuário
+# que são usados para armazenar informações e dados importantes.
+
+# As variáveis não podem começar com números.
+# Por exemplo: 3Num = 3 (isso não funciona como variável).
+
+print("Valor pra testes:", Num, type(Num))
+print("Nome:", Name, type(Name))
+print("Valor aprox. de Pi:", PiValue, type(PiValue))
+print("Já leu um livro hoje:", HaveReadABookToday, type(HaveReadABookToday))
+
+
+# Tipos de dados são identificados automaticamente pelo Python usando a função type()
+#Aqui alguns dos operadores aritmétricos
+
+print("Soma:", 5 + 3)
+print("Subtração:", 10 - 4)
+print("Multiplicação:", 6 * 2)
+print("Divisão:", 8 / 2)
+
+# Alguns operaores de atribuição
+
+print("\nOperadores de atribuição:")
+
+x = 10  # atribuição inicial
+print("Valor inicial de x:", x)
+
+x += 5  # x = x + 5
+print("Depois de x += 5:", x)
+
+x -= 3  # x = x - 3
+print("Depois de x -= 3:", x)
+
+x *= 2  # x = x * 2
+print("Depois de x *= 2:", x)
+
+x == 2  # x EXATAMENTE igual a 2
+print("Depois de x *= 2:", x)
+
+x != 2  # x indiferente de 2
+print("Depois de x *= 2:", x)
