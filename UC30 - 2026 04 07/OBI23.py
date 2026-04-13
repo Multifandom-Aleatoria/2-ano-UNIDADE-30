756378810e8c45cca9df8aa7d2b42b699c58c01d
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2026-04-13 16:03:25 -0300

    Add files via upload

---
+++ UC30 - 2026 04 07/OBI23.py
@@ -0,0 +1,17 @@
+P = int(input())
+B = int(input())
+D = int(input())
+
+Points = P * 1 + B * 2 + D * 3
+
+if Points >= 150:
+    print("Opção B")
+
+elif Points >= 120:
+    print("Opção B")
+
+elif Points >= 100:
+    print("Opção B")
+
+else:
+    print("Não tem")
