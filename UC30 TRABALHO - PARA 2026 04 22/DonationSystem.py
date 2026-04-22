2e0768ccae3cfd86124e0868ceaac39e22bb2ae1
Julinha/조셉 (ou José) <josevitorsilvestre10@gmail.com>
2026-04-22 08:52:41 -0300

    Add files via upload

---
+++ UC30 TRABALHO - PARA 2026 04 22/DonationSystem.py
@@ -0,0 +1,46 @@
+print("Hello World!")
+
+print("Bem vindo(a) ao sistema de doação para BKHbvqd!\nSomos uma organização criada para ajudar no combate à violência contra a mulher.\nPara isso precisamos da sua ajuda!\nAntes de começarmos uma doação, precisamos das informações básicas:\n")
+
+while True:
+    email = input("Digite seu email: ")
+    password = input("Agora digite sua senha: ")
+
+    print("Email:", email)
+    print("Senha:", password)
+
+    awnser = input("Tem certeza que é isso mesmo? S/N\n").strip().lower()
+
+    if awnser in ("sim", "s"):
+        print("Ótimo, vamos começar a doação!")
+        break
+    elif awnser in ("não", "nao", "n"):
+        print("Ok, vamos tentar de novo.")
+    else:
+        print("Resposta inválida. Por favor, responda com 'Sim' ou 'Não'.")
+
+#===================================================
+
+print("Quanto você gostaria de doar?")
+
+while True:
+    try:
+        amount = float(input("Digite o valor em R$: "))
+
+        if amount <= 0:
+            print("Valor inválido. Doação cancelada.")
+            print("Mesmo assim, sua participação foi tudo! :)")
+            break
+        else:
+            print("Obrigado pela doação de R$", amount, "!")
+            print("Doação feita!")
+            break
+
+    except ValueError:
+        print("Valor inválido. Por favor, digite um número.")
+
+print("\nObrigado(a) pela participação!")
+print("Sua doação ajuda a transformar o impossível em possível.")
+print("Feito por Júlia Vitória e Luan, estudantes de T.I. do Senac RN")
+
+# :)
